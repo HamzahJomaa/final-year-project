974e2090bf03b24decdb02a2edf6ca2dad509ad8
Hamzah Jomaa <hamzah.m.jomaa@gmail.com>
2022-02-02 00:11:53 +0200

    api

--- backend/app.js
+++ backend/app.js
@@ -15,7 +15,9 @@ app.use(cors())
 app.use(express.json({ limit: "20mb" }))
 app.use(express.urlencoded({ extended: false }))
 
-
+app.get("/",(req,res)=>{
+    res.send("Connected")
+})
 Router(app)
 
 
