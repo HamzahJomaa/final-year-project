ae5b3f63d642ecdff7d4a968307da31b80034486
Hamzah Jomaa <hamzah.m.jomaa@gmail.com>
2022-02-02 11:55:30 +0200

    dbConnection

--- backend/Config/connectDB.js
+++ backend/Config/connectDB.js
@@ -3,6 +3,7 @@ require("dotenv").config()
 
 exports.connectDb = (callback) => {
     const dbUrl = process.env.DB_URL || "mongodb://localhost:27017/finalyearproject"
+    console.log(dbUrl)
     mongoose.connect(`${dbUrl}`).then(() => {
             console.log("Database connected")
             callback()
