c28b08ca832cbcba47899dc215a6e1736af60cf3
Hamzah Jomaa <hamzah.m.jomaa@gmail.com>
2022-04-11 22:27:19 +0300

    ecosystem

---
+++ frontend-new/ecosystem.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    apps: [
+        {
+            name: 'next-site',
+            cwd: '/home/final-year-project/frontend-new/.next',
+            script: 'npm',
+            args: 'start',
+        },
+        // optionally a second project
+    ],
+};
